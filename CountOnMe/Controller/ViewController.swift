3fb1de509a14d277fc82bcfb9c3e7faf832b0114
huguesfils <huguesfils@icloud.com>
2019-12-08 19:34:33 +0100

    swift lint warnings done

--- CountOnMe/Controller/ViewController.swift
+++ CountOnMe/Controller/ViewController.swift
@@ -8,8 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController{
-    
+class ViewController: UIViewController {
     @IBOutlet weak var textView: UITextView!
     @IBOutlet var numberButtons: [UIButton]!
 
@@ -21,7 +20,7 @@ class ViewController: UIViewController{
         // Do any additional setup after loading the view.
         textView.text.removeAll()
     }
-    
+
     // View actions
     @IBAction func tappedNumberButton(_ sender: UIButton) {
         guard let numberText = sender.title(for: .normal) else {
@@ -31,82 +30,74 @@ class ViewController: UIViewController{
         if calculation.expressionHaveResult {
             textView.text = ""
         }
-        
         textView.text.append(numberText)
     }
-    
     @IBAction func tappedAdditionButton(_ sender: UIButton) {
         if calculation.canAddOperator {
             textView.text.append(" + ")
-            
         } else {
-            let alertVC = UIAlertController(title: "Zéro!", message: "Un operateur est déja mis !", preferredStyle: .alert)
+            let alertVC = UIAlertController(title: "Zéro!",
+                message: "Un operateur est déja mis !", preferredStyle: .alert)
             alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
             self.present(alertVC, animated: true, completion: nil)
         }
     }
-    
     @IBAction func tappedSubstractionButton(_ sender: UIButton) {
         if calculation.canAddOperator {
             textView.text.append(" - ")
         } else {
-            let alertVC = UIAlertController(title: "Zéro!", message: "Un operateur est déja mis !", preferredStyle: .alert)
+            let alertVC = UIAlertController(title: "Zéro!",
+                message: "Un operateur est déja mis !", preferredStyle: .alert)
             alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
             self.present(alertVC, animated: true, completion: nil)
         }
     }
 
-    @IBAction func tappedMultButton(_ sender: UIButton){
+    @IBAction func tappedMultButton(_ sender: UIButton) {
         if calculation.canAddOperator {
-                  textView.text.append(" x ")
-              } else {
-                  let alertVC = UIAlertController(title: "Zéro!", message: "Un operateur est déja mis !", preferredStyle: .alert)
-                  alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
-                  self.present(alertVC, animated: true, completion: nil)
-              }
+            textView.text.append(" x ")
+        } else {
+            let alertVC = UIAlertController(title: "Zéro!",
+                message: "Un operateur est déja mis !", preferredStyle: .alert)
+            alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+            self.present(alertVC, animated: true, completion: nil)
+        }
     }
-    
-    @IBAction func tappedDivButton(_ sender: UIButton){
-       if calculation.canAddOperator {
-                  textView.text.append(" / ")
-              } else {
-                  let alertVC = UIAlertController(title: "Zéro!", message: "Un operateur est déja mis !", preferredStyle: .alert)
-                  alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
-                  self.present(alertVC, animated: true, completion: nil)
-              }
+    @IBAction func tappedDivButton(_ sender: UIButton) {
+        if calculation.canAddOperator {
+            textView.text.append(" / ")
+        } else {
+            let alertVC = UIAlertController(title: "Zéro!",
+                message: "Un operateur est déja mis !", preferredStyle: .alert)
+            alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+            self.present(alertVC, animated: true, completion: nil)
+        }
     }
-    
-    @IBAction func reset (_sender: UIButton) {
+    @IBAction func reset (_ sender: UIButton) {
         textView.text.removeAll()
         calculation.reset()
     }
-    
-    
     @IBAction func tappedEqualButton(_ sender: UIButton) {
         calculation.elements = textView.text.split(separator: " ").map { "\($0)" }
-        
+
         guard calculation.expressionIsCorrect else {
-            let alertVC = UIAlertController(title: "Zéro!", message: "Entrez une expression correcte !", preferredStyle: .alert)
+            let alertVC = UIAlertController(title: "Zéro!",
+                message: "Entrez une expression correcte !", preferredStyle: .alert)
             alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
             return self.present(alertVC, animated: true, completion: nil)
         }
-        
-    
         guard calculation.expressionHaveEnoughElement else {
-            let alertVC = UIAlertController(title: "Zéro!", message: "Démarrez un nouveau calcul !", preferredStyle: .alert)
+            let alertVC = UIAlertController(title: "Zéro!",
+                message: "Démarrez un nouveau calcul !", preferredStyle: .alert)
             alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
             return self.present(alertVC, animated: true, completion: nil)
         }
-        
         guard !calculation.divideZero else {
-            let alertVC = UIAlertController(title: "Zéro!", message: "Division par zéro impossible", preferredStyle: .alert)
+            let alertVC = UIAlertController(title: "Zéro!",
+                message: "Division par zéro impossible", preferredStyle: .alert)
             alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
             return self.present(alertVC, animated: true, completion: nil)
         }
-        
-        
         textView.text.append(" = \(calculation.math())")
     }
-
 }
-
--- CountOnMe/Model.swift
+++ CountOnMe/Model.swift
@@ -8,10 +8,8 @@
 
 import Foundation
 
-extension Float
-{
-    var cleanValue: String
-    {
+extension Float {
+    var cleanValue: String {
         return self.truncatingRemainder(dividingBy: 1) == 0 ? String(format: "%.0f", self) : String(self)
     }
 }
@@ -21,33 +19,33 @@ class Calculation {
     var expressionIsCorrect: Bool {
         return elements.last != "+" && elements.last != "-"
     }
-    
+
     var divideZero: Bool {
         return elements.joined().contains("/0")
     }
-    
+
     var expressionHaveEnoughElement: Bool {
         return elements.count >= 3
     }
-    
+
     var canAddOperator: Bool {
         return elements.last != "+" && elements.last != "-"
     }
-    
+
     var elements: [String] = []
     var expressionHaveResult: Bool = false
-    
-    func math () -> String{
+
+    func math () -> String {
         // Create local copy of operations
         var operationsToReduce = simplifyMath(expression: elements)
-        
+
         // Iterate over operations while an operand still here
         while operationsToReduce.count > 1 {
-            
+
             let left = Float(operationsToReduce[0])!
             let operand = operationsToReduce[1]
             let right = Float(operationsToReduce[2])!
-            
+
             let result: Float
             switch operand {
             case "+": result = left + right
@@ -56,26 +54,26 @@ class Calculation {
             case "x":result = left * right
             default: fatalError("Unknown operator !")
             }
-            
             operationsToReduce = Array(operationsToReduce.dropFirst(3))
             operationsToReduce.insert("\(result.cleanValue)", at: 0)
         }
         return operationsToReduce.first!
     }
-    
-    func simplifyMath (expression: [String]) -> [String]{
+
+    func simplifyMath (expression: [String]) -> [String] {
         var operationToReduce = expression
         while operationToReduce.firstIndex(of: "x") != nil || operationToReduce.firstIndex(of: "/") != nil {
-            let operandIndex = operationToReduce.firstIndex(of: "x") ?? operationToReduce.firstIndex(of: "/")! // alors nil
-            
+            let operandIndex = operationToReduce.firstIndex(of: "x") ?? operationToReduce.firstIndex(of: "/")!
+            // alors nil
+
             let leftSide = Float(operationToReduce[operandIndex-1])! // transformation en float
             let rightSide = Float(operationToReduce[operandIndex+1])!
             let operand = operationToReduce[operandIndex]
-            
+
             let result: String
             if operand == "x"{
                 result = Float(leftSide*rightSide).cleanValue
-            }else{
+            } else {
                 result = Float(leftSide/rightSide).cleanValue
             }
             operationToReduce[operandIndex] = result
@@ -84,12 +82,8 @@ class Calculation {
         }
         return operationToReduce
     }
-    
-    func reset(){
+
+    func reset () {
         elements = [String]()
     }
 }
-
-
-
-
